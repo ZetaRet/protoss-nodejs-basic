41d337eb4756a50f96ae934a668e1810ed1c7a93
Ivo Yankulovski <ivoyankulovski@gmail.com>
2019-09-16 14:56:30 +0300

    Create XProtoSSChe.js

---
+++ modules/XProtoSSChe.js
@@ -0,0 +1,56 @@
+/**
+ * Author: Zeta Ret
+ * Date: 2019 - Today
+ * Extended ProtoSSChe Server loaded as module.
+ **/
+
+function getExtendedServerProtoSS(ProtoSSChe) {
+	return class XProtoSSChe extends ProtoSSChe {
+		constructor(routeCallback, routeScope, routeData) {
+			super();
+			var o = this;
+			o.routeCallback = routeCallback;
+			o.routeScope = routeScope;
+			o.routeData = routeData;
+			o.initRoute();
+		}
+
+		initRoute() {
+			var o = this;
+			return o;
+		}
+
+		onReadRequestBody(request, body, response) {
+			var o = this;
+			if (request.url) {
+				response.__splitUrl = o.splitUrl(request.url);
+			}
+			if (this.routeCallback) {
+				o.routeCallback.call(o.routeScope, o.routeData, body, request, response);
+			}
+			o.endResponse(request, response);
+			return o;
+		}
+
+		pushProtoSSResponse(request, response) {
+			var o = this;
+			return o;
+		}
+
+		addHeaders(request, response) {
+			var headers = {};
+			return headers;
+		}
+
+		endResponse(request, response) {
+			var o = this;
+			var input = response.__data.join("");
+			var headers = o.addHeaders(request, response);
+			response.writeHead(200, headers);
+			response.end(input);
+			return o;
+		}
+	}
+}
+
+module.exports.getExtendedServerProtoSS = getExtendedServerProtoSS;
