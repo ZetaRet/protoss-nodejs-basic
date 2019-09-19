f21b9f771b9d2099e0b3d3402d97f86a4860d68e
Ivo Yankulovski <ivoyankulovski@gmail.com>
2019-09-19 18:58:13 +0300

    Update modules
    +LobbyServer
    +stats.json examples

---
+++ modules/LobbyServer.js
@@ -0,0 +1,172 @@
+/**
+ * Author: Zeta Ret
+ * Date: 2019 - Today
+ * Lobby auth and rooms of Subserver.
+ **/
+
+var Subserver, xpros = require('./Subserver.js'),
+	events = require('events');
+
+class LobbyUser extends Array {
+	constructor() {
+		super();
+		var o = this;
+		o.lobby = null;
+		o.userId = null;
+		o.userData = {};
+	}
+
+	getUserPrerequisites() {
+		return o.userData.prerequisites;
+	}
+}
+
+class LobbyRoom extends Array {
+	constructor() {
+		super();
+		var o = this;
+		o.lobby = null;
+		o.roomId = null;
+		o.roomData = {};
+		o.isChat = false;
+		o.isLeader = false;
+	}
+}
+
+class LobbyApp extends Array {
+	constructor() {
+		super();
+		var o = this;
+		o.lobby = null;
+		o.appId = null;
+		o.appData = {};
+		o.requirements = {};
+	}
+
+	meetsRequirements(user) {
+		var o = this;
+		var v, k, pk, res = true,
+			pre = user.getUserPrerequisites();
+		for (k in o.requirements) {
+			v = o.requirements[k];
+			pk = pre[k];
+			if (pk) {
+				if (v.constructor === Function) res = v(k, pk, pre, user, o);
+				else res = (pk === v);
+			} else {
+				res = false;
+			}
+			if (!res) break;
+		}
+		return res;
+	}
+}
+
+function getExtendedServerProtoSS(ProtoSSChe) {
+	if (!Subserver) Subserver = xpros.getExtendedServerProtoSS(ProtoSSChe);
+	return class LobbyServer extends Subserver {
+		constructor() {
+			super();
+			var o = this;
+			o.lobbyId = null;
+			o.lobbyData = {};
+			o.apps = {};
+			o.users = {};
+			o.rooms = {};
+			o.lobbyEvents = new events.EventEmitter();
+			o.initLobby();
+			o.initRooms();
+			o.initApps();
+		}
+
+		initLobby() {
+			var o = this;
+			o.lobbyId = o.rndstr(10);
+		}
+
+		initRooms() {}
+
+		initApps() {}
+
+		connectTo(options, data) {
+			var o = this;
+			var req = o.htserv.request(options, o.onConnectedToServer);
+			req.on('error', o.onConnectError);
+			if (data) req.write(data);
+			req.end();
+			return req;
+		}
+
+		onConnectError(e) {}
+
+		onConnected(res) {}
+
+		lobby(data) {
+			var o = this;
+			var k;
+			for (k in data) o.lobbyData[k] = data[k];
+			o.lobbyEvents.emit("lobbyUpdate", o);
+			return o;
+		}
+
+		updateRemoveUser(u, update, remove) {
+			var o = this;
+			if (update) o.lobbyEvents.emit("updateUser", u, o);
+			if (remove) o.lobbyEvents.emit("removeUser", u, o);
+			return o;
+		}
+
+		updateRemoveRoom(r, update, remove) {
+			var o = this;
+			if (update) o.lobbyEvents.emit("updateRoom", r, o);
+			if (remove) o.lobbyEvents.emit("removeRoom", r, o);
+			return o;
+		}
+
+		updateRemoveApp(a, update, remove) {
+			var o = this;
+			if (update) o.lobbyEvents.emit("updateApp", a, o);
+			if (remove) o.lobbyEvents.emit("removeApp", a, o);
+			return o;
+		}
+
+		user(userId, usercls) {
+			var o = this;
+			if (!usercls) usercls = LobbyUser;
+			var u = new usercls();
+			u.lobby = o.lobbyId;
+			u.userId = userId;
+			o.users[userId] = u;
+			o.lobbyEvents.emit("newUser", u, o);
+			return u;
+		}
+
+		room(roomId, roomcls) {
+			var o = this;
+			if (!roomcls) roomcls = LobbyRoom;
+			var r = new roomcls();
+			r.lobby = o.lobbyId;
+			r.roomId = roomId;
+			o.rooms[roomId] = r;
+			o.lobbyEvents.emit("newRoom", r, o);
+			return r;
+		}
+
+		app(appId, appcls) {
+			var o = this;
+			if (!appcls) appcls = LobbyApp;
+			var a = new appcls();
+			a.lobby = o.lobbyId;
+			a.appId = appId;
+			o.apps[appId] = a;
+			o.lobbyEvents.emit("newApp", a, o);
+			return a;
+		}
+
+	}
+}
+
+module.exports.lobbyUserClass = LobbyUser;
+module.exports.lobbyRoomClass = LobbyRoom;
+module.exports.lobbyAppClass = LobbyApp;
+module.exports.getExtendedServerProtoSS = getExtendedServerProtoSS;
---
+++ modules/LobbyServer.md
@@ -0,0 +1,134 @@
+> __Author: Zeta Ret__  
+> __Lobby auth and rooms of Subserver__  
+# Distribute users after authentication into rooms or applications based on requirements  
+> *Requires: events*  
+> *Version: 1.2.1*  
+> *Date: 2019 - Today*  
+
+__required*__
+
+## zetaret.node.modules::LobbyServer  
+> Inherits: [zetaret.node.modules::SubServer](Subserver.md)  
+
+### *Properties*  
+
+#  
+__lobbyId__ String  
+default null,   
+
+#  
+__lobbyData__ Object  
+default {},   
+
+#  
+__apps__ Object  
+default {},   
+
+#  
+__users__ Object  
+default {},   
+
+#  
+__rooms__ Object  
+default {},   
+
+#  
+__lobbyEvents__ events.EventEmitter  
+default new,   
+
+
+##  
+### *Methods*  
+
+##  
+__LobbyServer() : *void*__  
+implement application based lobby logic using provided objects  
+> *return __void__*  
+
+##  
+__initLobby() : *void*__  
+  
+> *return __void__*  
+
+##  
+__initRooms() : *void*__  
+  
+> *return __void__*  
+
+##  
+__initApps() : *void*__  
+  
+> *return __void__*  
+
+##  
+__connectTo(*Object* options, *String* data) : *http.ClientRequest*__  
+connect to another server or lobby  
+- __options*__ - __*Object*__, default http server options per request  
+- data - __*String*__, data to send with request  
+> *return __http.ClientRequest__, ends automatically*  
+
+##  
+__onConnectError(*Error* e) : *void*__  
+  
+- __e*__ - __*Error*__,   
+> *return __void__*  
+
+##  
+__onConnected(*http.ServerResponse* res) : *void*__  
+  
+- __res*__ - __*http.ServerResponse*__,   
+> *return __void__*  
+
+##  
+__lobby(*Object* data) : *zetaret.node.modules::LobbyServer*__  
+update current lobby server data  
+- __data*__ - __*Object*__,   
+> *return __zetaret.node.modules::LobbyServer__*  
+
+##  
+__updateRemoveUser(*LobbyUser* u, *Boolean* update, *Boolean* remove) : *zetaret.node.modules::LobbyServer*__  
+  
+- __u*__ - __*LobbyUser*__,   
+- update - __*Boolean*__,   
+- remove - __*Boolean*__,   
+> *return __zetaret.node.modules::LobbyServer__*  
+
+##  
+__updateRemoveRoom(*LobbyRoom* r, *Boolean* update, *Boolean* remove) : *zetaret.node.modules::LobbyServer*__  
+  
+- __r*__ - __*LobbyRoom*__,   
+- update - __*Boolean*__,   
+- remove - __*Boolean*__,   
+> *return __zetaret.node.modules::LobbyServer__*  
+
+##  
+__updateRemoveApp(*LobbyApp* a, *Boolean* update, *Boolean* remove) : *zetaret.node.modules::LobbyServer*__  
+  
+- __a*__ - __*LobbyApp*__,   
+- update - __*Boolean*__,   
+- remove - __*Boolean*__,   
+> *return __zetaret.node.modules::LobbyServer__*  
+
+##  
+__user(*String* userId, *Function* usercls) : *LobbyUser*__  
+add new user to lobby using default/custom Class  
+- __userId*__ - __*String*__,   
+- usercls - __*Function*__,   
+> *return __LobbyUser__*  
+
+##  
+__room(*String* roomId, *Function* roomcls) : *LobbyRoom*__  
+add new room to lobby using default/custom Class  
+- __roomId*__ - __*String*__,   
+- roomcls - __*Function*__,   
+> *return __LobbyRoom__*  
+
+##  
+__app(*String* appId, *Function* appcls) : *LobbyApp*__  
+add new app to lobby using default/custom Class  
+- __appId*__ - __*String*__,   
+- appcls - __*Function*__,   
+> *return __LobbyApp__*  
+
+---  
+### MarkDown - JsonDox 1.02 - Zeta Ret Zetadmin Documentation Generator
