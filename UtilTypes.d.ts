61ca1b8ebc3eca291519aad1187045f45dbf01c4
Ivo Yankulovski <ivoyankulovski@gmail.com>
2020-07-12 18:41:55 +0300

    Delete UtilTypes.d.ts

--- UtilTypes.d.ts
+++
@@ -1,7 +0,0 @@
-declare type UtilsEventEmitter = import("events").EventEmitter;
-declare type UtilsFSWatcher = import("fs").FSWatcher;
-
-declare namespace zetaret.node.utils {
-	export type Emitter = UtilsEventEmitter;
-	export type Watcher = UtilsFSWatcher;
-}
