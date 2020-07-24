2c35cd1d3d8583245b5dd99078c804c12c7b27d8
Ivo Yankulovski <ivoyankulovski@gmail.com>
2020-07-24 16:46:50 +0300

    Update project

--- indexUtilsTS.ts
+++ indexUtilsTS.ts
@@ -3,6 +3,7 @@ import { HTMLParser } from "./utils/html/HTMLParser";
 import { HTMLDomElement } from "./utils/html/HTMLDomElement";
 import * as RequireSupernameModule from "./utils/nano/RequireSupername";
 import { StringLength } from "./utils/nano/StringLength";
+import { Cookies } from "./utils/web/Cookies";
 
 const HTMLCacheCTOR: zetaret.node.utils.html.HTMLCacheCTOR = HTMLCache as any;
 const HTMLCacheInstance: zetaret.node.utils.html.HTMLCache = new HTMLCacheCTOR();
@@ -13,11 +14,14 @@ const HTMLDomElementInstance: zetaret.node.utils.html.HTMLDomElement = new HTMLD
 const StringLengthCTOR: zetaret.node.utils.nano.StringLengthCTOR = StringLength as any;
 const StringLengthInstance: zetaret.node.utils.nano.StringLength = new StringLengthCTOR("");
 const RequireSupernameModuleLoaded: zetaret.node.utils.nano.RequireSupernameModule = RequireSupernameModule as any;
+const CookiesCTOR: zetaret.node.utils.web.CookiesCTOR = Cookies as any;
+const CookiesInstance: zetaret.node.utils.web.Cookies = new CookiesCTOR();
 
 [
 	HTMLCacheInstance,
 	HTMLParserInstance,
 	HTMLDomElementInstance,
 	StringLengthInstance,
-	RequireSupernameModuleLoaded
+	RequireSupernameModuleLoaded,
+	CookiesInstance
 ].forEach((e: any) => console.log(e));
