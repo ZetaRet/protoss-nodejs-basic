7af39b83fba726826f86adf78e8b38df5642ae62
Ivo Yankulovski <ivoyankulovski@gmail.com>
2020-11-20 16:14:23 +0200

    Add APIController.ts

---
+++ api/APIController.ts
@@ -0,0 +1,34 @@
+class APIController implements zetaret.node.api.APIController {
+	public ctype: string;
+	public api: Array<string>;
+	public server: zetaret.node.modules.Subserver;
+	public db: object;
+	public apiPrefix: string;
+
+	constructor() {
+		this.ctype = 'application/json';
+		this.api = [];
+		this.server = null;
+		this.db = {};
+		this.apiPrefix = 'api/';
+	}
+
+	public addServer(server: zetaret.node.modules.Subserver): void {
+		const o: APIController = this;
+		o.server = server;
+		o.api.forEach((e: string) => server.addPathListener(o.apiPrefix + e, (o as any)[e].bind(o)));
+	}
+
+	public setHeaders(response: zetaret.node.RoutedResponse): void {
+		if (response.__headers)
+			(response.__headers as any)['content-type'] = this.ctype;
+	}
+
+	public pushData(response: zetaret.node.RoutedResponse, data: object): void {
+		this.setHeaders(response);
+		if (response.__rawdata) response.__rawdata.push(data);
+		else response.__data.push(JSON.stringify(data));
+	}
+}
+
+module.exports.APIController = APIController;
